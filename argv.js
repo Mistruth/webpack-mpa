394a4147f5ca00fa9abd836f9fc1294e096dbc58
v_zhangwenzhao <v_zhangwenzhao@bilibili.com>
2019-02-14 14:24:27 +0800

    fix:devserve

--- argv.js
+++ argv.js
@@ -1,3 +1,15 @@
 const paramsArgv = [... process.argv]
-const params = paramsArgv.slice(2)
+
+let params = []
+
+if (!isDevServer(paramsArgv)) {
+  params = paramsArgv.slice(2)
+} else {
+  params = paramsArgv.slice(6)
+}
+
+function isDevServer(argvArray) {
+  return argvArray.some(argv => argv.includes('webpack.dev.conf'))
+}
+
 module.exports = params
